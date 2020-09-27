2ba43235771329bfd6660002bdb3ca64d1c261e0
VSRUJANA <srujana.valavala@gmail.com>
2020-09-27 17:44:29 +0530

    [srujana] Refactor. Check Equality of 2 lines program added (UC-2)

--- Line_Comparison_Program/Program.cs
+++ Line_Comparison_Program/Program.cs
@@ -4,13 +4,18 @@ namespace Line_Comparison_Program
 {
     class Program
     {
-
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Computation Program");
-            Console.WriteLine("Length of line: " + LengthOfLine());
+            double lengthOfLine1 = LengthOfLine();
+            Console.WriteLine("Length of line 1: " + lengthOfLine1);
+            double lengthOfLine2 = LengthOfLine();
+            Console.WriteLine("Length of line 2: " + lengthOfLine2);
+            if (lengthOfLine1 == lengthOfLine2)
+                Console.WriteLine("Both the lines are equal");
+            else
+                Console.WriteLine("Both the lines are unequal");
         }
-
         static double LengthOfLine()
         {
             double[] x = new double[2];
@@ -22,11 +27,12 @@ namespace Line_Comparison_Program
                 x[index] = random.Next(-100, 100);
                 y[index] = random.Next(-100, 100);
             }
-            Console.WriteLine("Coordinates of the line : ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
+            Console.WriteLine("Coordinates of the line are ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
+
 
             double length = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
             return Math.Round(length, 2);
-        }
 
+        }
     }
 }
