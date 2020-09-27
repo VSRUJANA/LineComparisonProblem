4cf225c6e39b26613a6cfe8e4c18d10d44ffdb61
VSRUJANA <srujana.valavala@gmail.com>
2020-09-27 17:51:33 +0530

    [srujana] Refactor. Compare 2 lines program added (UC-3)

--- Line_Comparison_Program/Program.cs
+++ Line_Comparison_Program/Program.cs
@@ -11,10 +11,13 @@ namespace Line_Comparison_Program
             Console.WriteLine("Length of line 1: " + lengthOfLine1);
             double lengthOfLine2 = LengthOfLine();
             Console.WriteLine("Length of line 2: " + lengthOfLine2);
-            if (lengthOfLine1 == lengthOfLine2)
-                Console.WriteLine("Both the lines are equal");
+            double res = lengthOfLine1.CompareTo(lengthOfLine2);
+            if (res > 0)
+                Console.WriteLine("line 1 is greater than line 2");
+            else if (res < 0)
+                Console.WriteLine("line 1 is smaller than line 2");
             else
-                Console.WriteLine("Both the lines are unequal");
+                Console.WriteLine("Both the lines are equal");
         }
         static double LengthOfLine()
         {
@@ -28,8 +31,6 @@ namespace Line_Comparison_Program
                 y[index] = random.Next(-100, 100);
             }
             Console.WriteLine("Coordinates of the line are ({0}, {1}) & ({2}, {3})", x[0], y[0], x[1], y[1]);
-
-
             double length = Math.Sqrt(Math.Pow((x[1] - x[0]), 2) + Math.Pow((y[1] - y[0]), 2));
             return Math.Round(length, 2);
 
